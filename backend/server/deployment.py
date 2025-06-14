e3285a63243617873ba301641d85f7548fa3eafc
Bunjiro Yamada <yamadab@bu.edu>
2025-06-14 15:07:15 -0400

    got rid of WEBSITE_HOSTNAME

--- backend/server/deployment.py
+++ backend/server/deployment.py
@@ -2,8 +2,6 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
 SECRET_KEY = os.environ['SECRET_KEY']
 
